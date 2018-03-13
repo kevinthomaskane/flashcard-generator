752eda6d7e9307858d91898d95700bb6d1b7ab61
Kevin Kane <kevinthomaskane@gmail.com>
2018-03-12 20:13:27 -0700

    created function to ask questions and save user input

--- ClozeCard.js
+++ ClozeCard.js
@@ -3,13 +3,15 @@
 
 
 function ClozeCard(text, cloze){
+    this.partial = text;
+    this.cloze = cloze;
     this.fullText = function(){
         var part2 = text.split(".").splice(3,1)
         var full = cloze + " " + part2
         return full
     }
     ;
-    this.cloze = cloze;
-    this.partial = text;
+    
+    
 }
 module.exports = ClozeCard;
--- main.js
+++ main.js
@@ -1,71 +1,84 @@
 
-
-var questions = [firstPresident, firstPresidentCloze, finalsMVP, finalsMVPCloze, billRussell, billRussellCloze, coachingTitles, coachingTitlesCloze, shortest, shortestCloze, hornets, hornetsCloze, ppg, ppgCloze, record, recordCloze]
 var BasicCard = require("./BasicCard.js")
 var ClozeCard = require("./ClozeCard.js")
 var inquirer = require("inquirer")
-var current
-
-function chooseQuestion(){
-    indexChoice = Math.floor(Math.random*16)+1
-    current = questions[indexChoice];
-    return current
-}
-
-
-inquier.prompt([
-    {
-        type: "input",
-        name: "userInput",
-        message: chooseQuestion().current[Object.keys(current)[0]]
-      }
-]).then(function(userInput){
-    console.log(userInput)
-})
 
 var firstPresident = new BasicCard(
-    "Who was the first president of the United States?", "George Washington");
+    "Who has scored the most points in an NBA game?", "Wilt Chamberlain");
 var firstPresidentCloze = new ClozeCard(
-   "...was the first president of the United States.",  "George Washington");
+    "...has scored the most points in an NBA game.", "Wilt Chamberlain");
 
 var finalsMVP = new BasicCard(
-    "Who has won the most NBA finals MVP awards", "Michael Jordan");
+    "Who has won the most NBA finals MVP awards?", "Michael Jordan");
 var finalsMVPCloze = new ClozeCard(
-    "...has won the most NBA finals MVP awards", "Michael Jordan" 
+    "...has won the most NBA finals MVP awards.", "Michael Jordan"
 )
 
 var billRussell = new BasicCard(
-    "Who is the only player to win 2 championships as a player-coach", "Bill Russell");
+    "Who is the only player to win 2 championships as a player-coach?", "Bill Russell");
 var billRussellCloze = new ClozeCard(
-     "...is the only player to win 2 championships as a player-coach", "Bill Russell"
+    "...is the only player to win 2 championships as a player-coach.", "Bill Russell"
 )
 
 var coachingTitles = new BasicCard(
-    "Who has won the most NBA coaching titles", "Phil Jackson");
+    "Who has won the most NBA coaching titles?", "Phil Jackson");
 var coachingTitlesCloze = new ClozeCard(
-    "...has won the most NBA coaching titles", "Phil Jackson"
+    "...has won the most NBA coaching titles.", "Phil Jackson"
 )
 
 var shortest = new BasicCard(
-    "Who is the shortest player of all-time", "Mugsy Bogues");
+    "Who is the shortest player of all-time?", "Mugsy Bogues");
 var shortestCloze = new ClozeCard(
-     "...is the shortest player of all-time", "Mugsy Bogues"
+    "...is the shortest player of all-time.", "Mugsy Bogues"
 )
 
 var hornets = new BasicCard(
-    "Which team drafted Kobe Bryant", " Charlotte Hornets");
+    "Which team drafted Kobe Bryant?", " Charlotte Hornets");
 var hornetsCloze = new ClozeCard(
-     "...drafted Kobe Bryant", "Charlotte Hornets"
+    "...drafted Kobe Bryant.", "Charlotte Hornets"
 )
 
 var ppg = new BasicCard(
-    "Which player has the highest career ppg", "Michael Jordan");
+    "Which player has the highest career ppg?", "Michael Jordan");
 var ppgCloze = new ClozeCard(
-     "...has the highest career ppg", "Michael Jordan"
+    "...has the highest career ppg.", "Michael Jordan"
 )
 
 var record = new BasicCard(
-    "Which team has the best record in a single season", "Golden State");
+    "Which team has the best record in a single season?", "Golden State");
 var recordCloze = new ClozeCard(
-     "...has the best record in a single season", "Golden State"
+    "...has the best record in a single season.", "Golden State"
 )
+
+var questions = [firstPresident, firstPresidentCloze, finalsMVP, finalsMVPCloze, billRussell, billRussellCloze, coachingTitles, coachingTitlesCloze, shortest, shortestCloze, hornets, hornetsCloze, ppg, ppgCloze, record, recordCloze]
+
+var current;
+function chooseQuestion() {
+    var indexChoice = Math.floor(Math.random() * 16 + 1)
+    current = questions[indexChoice];
+    return current[Object.keys(current)[0]]
+}
+
+
+function ask() {
+    var choice = chooseQuestion();
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "userInput",
+            message: choice
+        }
+    ]).then(function (user) {
+        console.log(user.userInput)
+        if (user.userInput === current[Object.keys(current)[1]]) {
+            console.log("correct")
+            ask();
+        }
+        else {
+            console.log("sorry the answer was " + current[Object.keys(current)[1]])
+            ask();
+        }
+    })
+}
+
+ask();
